82535d4366a453344851c044e47cc7645dd29758
WRLDExplorer <wownewexplorer@gmail.com>
2026-08-26 23:01:25 +0200

    v0.0.2 for 1.21.11

--- src/client/java/com/explorer/locatordisplay/client/LocatorColorUtil.java
+++ src/client/java/com/explorer/locatordisplay/client/LocatorColorUtil.java
@@ -1,24 +1,18 @@
 package com.explorer.locatordisplay.client;
 
 import java.util.UUID;
+import net.minecraft.util.math.ColorHelper;
 
 public class LocatorColorUtil {
+    /**
+     * Packs 255 Alpha onto the UUID hashCode integer and adjusts brightness
+     * @param uuid
+     * @return color with stripped alpha
+     */
 
     public static int getColorFromUuid(UUID uuid) {
-        // get x64 halfs
-        long mostSigBits = uuid.getMostSignificantBits();
-        long leastSigBits = uuid.getLeastSignificantBits();
-
-        // combine them w/ XOR
-        long combined = mostSigBits ^ leastSigBits;
-        int hash = (int)(combined ^ (combined >>> 32));
-
-        // get rgb from the low 24 bits of the hash
-        int red = (hash >> 16) & 0xFF;
-        int green = (hash >> 8) & 0xFF;
-        int blue = hash & 0xFF;
-
-        // return packed 0xRRGGBB integer
-        return (red << 16) | (green << 8) | blue;
+        int argbColor = ColorHelper.fullAlpha(uuid.hashCode());
+        int colorWithBrightness = ColorHelper.withBrightness(argbColor, 0.9F);
+        return colorWithBrightness & 0xFFFFFF;
     }
 }
